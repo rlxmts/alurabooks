82db39bd8c3ac8cad7fb0d11f81c9ba56dfbbf4c
Matheus Cruz <165737916+rlxmts@users.noreply.github.com>
2024-06-13 15:04:55 -0300

    :recycle: Refatorando CSS: Criando Estilo Global

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const StyleGlobal = createGlobalStyle`
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    text-transform: none;
+    list-style: none;
+  }
+
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+`
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StyleGlobal />
     <App />
   </React.StrictMode>
 );
